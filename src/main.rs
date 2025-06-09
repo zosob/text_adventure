52cff71fddb9f60f1e97b529d9faefac96c9593d
Bhaskar Ghosh <bhaskar4647@gmail.com>
2025-06-09 11:33:22 -0500

    Add player, item, and room structs.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,36 @@
 use std::io::{self, Write};
 
+#[derive(Debug)]
+struct Player{
+    name: String,
+    health: i32,
+    inventory: Vec<Item>,
+}
+
+#[derive(Debug)]
+enum Item{
+    Potion, 
+    Key,
+    Sword,
+}
+
+#[derive(Debug)]
+struct Room {
+    description: String,
+    has_enemy: bool,
+    items: Vec<Item>,
+}
+
 fn main() {
-    println!("Welcome to the Dungeion Adventure!");
+
+    let mut player = Player{
+        name: String::from("Hero"),
+        health: 100,
+        inventory: vec![],
+    };
+
+    println!("Welcome to the Dungeon Adventure!");
+    println!("You are {}, with {} health.", player.name, player.health);
     println!("Type 'help' to see available commands");
 
     loop {
@@ -15,6 +44,16 @@ fn main() {
                 let command = input.trim().to_lowercase();
 
                 match command.as_str(){
+                    "inventory" => {
+                        if player.inventory.is_empty(){
+                            println!("Your inventoryt is empty!");
+                        } else {
+                            println!("You have: ");
+                            for item in &player.inventory{
+                                println!("{:?}", item);
+                            }
+                        }
+                    }
                     "help" => print_help(),
                     "look" => println!("You see a dark room with a flickering torch."),
                     "quit" => {
@@ -33,6 +72,7 @@ fn print_help(){
     
     println!("Availble commands:");
     println!("look - Describe the current room");
+    println!("inventory - Show your items");
     println!("quit - Exit the game");
     println!("help - Show this help message");
 }
